155fd784bd23edb728809e06b17fa66dec84dd99
MollyGann <27msokol@gannacademy.org>
2024-12-09 12:05:29 -0500

    working on display hit writing

--- src/App.java
+++ src/App.java
@@ -5,6 +5,7 @@ import processing.core.*;
 public class App extends PApplet {
     ArrayList<Fruit> fruits;
     private int livesleft = 3;
+    private int scoreofhitfruit = 0;
     PVector[] circlesswordparts = { new PVector(-1200, 100), new PVector(-1200, 100), new PVector(-1200, 100) };
 
     public static void main(String[] args) {
@@ -15,10 +16,6 @@ public class App extends PApplet {
     public void setup() {
         fruits = new ArrayList<>();
 
-        Fruit applefruits = new Fruit("apple", 50, this);
-        // Fruit kiwifruits = new Fruit("kiwi", 250, 290, this);
-        // fruits.add(kiwifruits);
-        fruits.add(applefruits);
         background(59, 35, 6);
 
     }
@@ -30,23 +27,16 @@ public class App extends PApplet {
     public void draw() {
         background(59, 35, 6);
 
-        for (Fruit f : fruits) {
-            f.display();
-        }
-        if (circlesswordparts[0] != null) {
-            for (int i = 0; i < circlesswordparts.length; i++) {
-                fill(i * 100);
-                PVector p = circlesswordparts[i];
+        displayFruit();
 
-                circle(p.x, p.y, 30 - 5 * i);
+        displaySword();
 
-            }
-
-        }
-        if (frameCount % 2 == 0) {
-            update();
-
-        }
+        fill(255);
+        textSize(40);
+        text("Lives: " + livesleft, 450, 30);
+        fill(255);
+        textSize(40);
+        text("Score: " + scoreofhitfruit, 450, 60);
 
     }
 
@@ -56,17 +46,91 @@ public class App extends PApplet {
         circlesswordparts[2] = circlesswordparts[1];
         circlesswordparts[1] = circlesswordparts[0];
         circlesswordparts[0] = mouse;
+
     }
 
     public void mouseTouch() {
         for (int i = 0; i < fruits.size(); i++) {
             Fruit f = fruits.get(i);
-            if (!f.checkTouch(mouseX, mouseY)){
-                livesleft--;
-        }
+            if (!f.checkTouch(mouseX, mouseY)) {
+                fruits.remove(f);
+                scoreofhitfruit++;
+                
+            }
         }
     }
 
-    
+    public void displayFruit() {
+
+       hittingBottom();
+
+        createFruit();
+    }
+
+    public void displaySword() {
+        if (circlesswordparts[0] != null) {
+            for (int i = 0; i < circlesswordparts.length; i++) {
+                fill(i * 100);
+                PVector p = circlesswordparts[i];
+
+                circle(p.x, p.y, 15 +(5 * i));
+
+            }
+
+        }
+        if (frameCount % 2 == 0) {
+            update();
+
+        }
+    }
+
+    public void keyPressed() {
+        if (key == ' ') {
+            fruits.add(new Fruit("apple", this));
+        }
+    }
+
+    public void hittingBottom(){
+        for (int i = 0; i < fruits.size(); i++) {
+            Fruit f = fruits.get(i);
+            f.display();
+            if (f.hitBottom()) {
+                System.out.println("off screen");
+                livesleft--;
+                fruits.remove(f);
+                i--;
+            }
+        }
+        System.out.println(fruits.size());
+    }
+
+    public void createFruit(){
+        if (frameCount % 100 == 0) {
+            if (random(1) < .5) { //change so quantity is based on height
+                for (int i = 0; i < 3; i++) {
+                    if (random(1) < .5) {
+                        fruits.add(new Fruit("kiwi", this));
+
+                    } else {
+                        fruits.add(new Fruit("apple", this));
+
+                    }
+
+                }
+            } else {
+                for (int i = 0; i < 1; i++) {
+                    if (random(1) < .5) {
+                        fruits.add(new Fruit("kiwi", this));
+
+                    } else {
+                        fruits.add(new Fruit("apple", this));
+
+                    }
+
+                }
+            }
+
+        }
+    }
 
 }
--- src/Fruit.java
+++ src/Fruit.java
@@ -1,6 +1,6 @@
 import processing.core.PApplet;
 
-public class Fruit {
+public class Fruit {   //list of problems: balls getting out before leaving bottom, the game too hard, broken pieces
     public float x;
     public float y;
 private int color;
@@ -14,7 +14,8 @@ private boolean AliveFruit = true;
 
 
 
-public Fruit(String specificfruitname, int ypos, PApplet c ){
+
+public Fruit(String specificfruitname, PApplet c ){
     x = c.random(0,800);
    if(x<400){
         xVelocity = c.random(0,5);
@@ -29,16 +30,17 @@ public Fruit(String specificfruitname, int ypos, PApplet c ){
     if(fruitname.equals("apple")){
         appleSetup();
     }
-    yVelocity = -15;
+    yVelocity = canvas.random(-15, -10);
     
-    
+   
 
    
     }
     
 public void display(){
-   launchBall();
   
+if(AliveFruit){
+
 
     if (fruitname.equals("apple")){
         appleSetup();
@@ -46,25 +48,38 @@ public void display(){
         kiwiVisiual();
     }
 
-    canvas.circle(x, y, size);
+   launchBall(); 
+        color = canvas.color(255, 0, 0);
+
+}
+}
+// else{
+//     launchBall();
+//      canvas.circle(x,y, 10);
+//     canvas.circle(x,y, 10);
+//     canvas.circle(x,y, 10);
+//     canvas.circle(x,y, 10);
+//     canvas.circle(x,y, 10);
+
+// }
+   
 
    
        
-        color = canvas.color(255, 0, 0);
 
     
-}
 
 public void appleSetup(){
     color = canvas.color(168, 5, 5);
     canvas.fill(color);
+    canvas.circle(x, y, size);
+
     size = 75;
 
 }
 
 public void kiwiVisiual(){
-    canvas.fill(color);
-    color = canvas.color(5, 84, 17);
+    canvas.fill(5,84,17);
     canvas.circle(x, y, size);
     size = 50;
 
@@ -75,20 +90,46 @@ public void kiwiVisiual(){
 
 public boolean checkTouch(int mouseX, int mouseY) {
     float distanceFromCenter = canvas.dist(x, y, mouseX, mouseY);
-    if (distanceFromCenter < size / 2) {
+    if (distanceFromCenter < size / 2 ) {
         System.out.println("dead");
-        gravity = 1.3f;
+AliveFruit = false;
+int starttime = canvas.frameCount;
+System.out.println(starttime);
+
+// yVelocity =6;
+       
+        if (canvas.frameCount -starttime < 50) { 
+            System.out.println(canvas.frameCount);
+            canvas.fill(color);
+            canvas.textSize(32);
+            canvas.text("+ 1 POINT!", x, y);}
+
         return false;
     } else {
-        System.out.println("alive");
-        return true;
-    }
+       
+    }        return true;
+
 }
 
-private void launchBall(){
+public void launchBall(){
     yVelocity += gravity;
     y += yVelocity;
     x+= xVelocity;
+
 }
 
+
+
+public boolean hitBottom(){
+    // System.out.println("checking bottom");
+    if (y + size / 2 > canvas.height ){
+        // AliveFruit = false;
+        System.out.println("hit bottom"); 
+        return true;
+        }     
+else{
+    return false;
+}
+       
+}
 }
